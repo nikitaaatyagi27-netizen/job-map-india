eba56931d3fddc509dff1838f6f147bfff7e37ac
Nikita Tyagi <nikitaaatyagi27@gmail.com>
2026-06-03 01:09:24 +0530

    Remove dead code: unused components and debug scripts

--- server/testdns.js
+++
@@ -1,5 +0,0 @@
-const dns = require('dns');
-dns.resolveSrv('_mongodb._tcp.cluster0.oqngujy.mongodb.net', (err, a) => {
-  if (err) console.log('SRV FAILED:', err.message);
-  else console.log('SRV OK:', JSON.stringify(a[0]));
-});
